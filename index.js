d10b1929be4c96e6442bfa152f8cf4c23c235856
Don Justine Fontanilla <djjustinekier@gmail.com>
2017-09-07 13:22:04 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,43 @@
+var katzDeliLine = [];
+
+function takeANumber(newArr, newPersonName) {
+  newArr.push(newPersonName);
+  for (var  x = 0; x <= newArr.length; x++) {
+    if (newArr[x] === newPersonName) {
+      var personNumber = x + 1;
+      return "Welcome, " + newPersonName + ". You are number " + personNumber + " in line.";
+    }
+
+}
+}
+
+function nowServing(newArr) {
+  if (newArr.length === 0) {
+      return 'There is nobody waiting to be served!';
+    }
+  else {
+    	var first = newArr.shift();
+      	return "Currently serving " + first + ".";
+    }
+    newArr;
+}
+
+function currentLine(newArr) {
+  if (newArr.length === 0) {
+    return "The line is currently empty.";
+  }
+  else {
+    
+    for (var x = 0; x < newArr.length; x++){
+      var number = x + 1;
+    var  name += x + ". " + newArr[x] + ",";
+    }
+    return "The line is currently: " + name;
+  }
+}
+
+console.log(takeANumber(katzDeliLine,"ako"));
+console.log(takeANumber(katzDeliLine,"me"));
+console.log(takeANumber(katzDeliLine,"kier"));
+console.log(nowServing(katzDeliLine));
+console.log(nowServing(katzDeliLine));
